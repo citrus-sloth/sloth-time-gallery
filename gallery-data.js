343a477298d633692777cab1b47f85a69ee37a11
シトラス <citrus.swc.yukkuri@gmail.com>
2026-07-15 16:17:11 +0900

    Update Slow Time Gallery

---
+++ gallery-data.js
@@ -0,0 +1,210 @@
+// Sloth Time Gallery - Artwork Data
+// This is the single source of truth for all artwork information
+
+const galleryData = [
+    // Morning Room
+    {
+        image: "Morning/all34.png",
+        room: "morning",
+        order: 1,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Morning/all09.png",
+        room: "morning",
+        order: 2,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Morning/all10.png",
+        room: "morning",
+        order: 3,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Morning/all12.png",
+        room: "morning",
+        order: 4,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Morning/all16.jpg",
+        room: "morning",
+        order: 5,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Morning/all17.jpg",
+        room: "morning",
+        order: 6,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Morning/all18.jpg",
+        room: "morning",
+        order: 7,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Morning/all19.jpg",
+        room: "morning",
+        order: 8,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Morning/all20.jpg",
+        room: "morning",
+        order: 9,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Morning/all21.jpg",
+        room: "morning",
+        order: 10,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Morning/all22.jpg",
+        room: "morning",
+        order: 11,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Morning/all23.jpg",
+        room: "morning",
+        order: 12,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Morning/all26.png",
+        room: "morning",
+        order: 13,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Morning/all27.png",
+        room: "morning",
+        order: 14,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Morning/all29.png",
+        room: "morning",
+        order: 15,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Morning/all30.png",
+        room: "morning",
+        order: 16,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Morning/all31.png",
+        room: "morning",
+        order: 17,
+        title: "",
+        caption: "",
+        date: ""
+    },
+
+    // Day Room
+    {
+        image: "Day/all01.jpeg",
+        room: "day",
+        order: 1,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Day/all15.png",
+        room: "day",
+        order: 2,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Day/all13.png",
+        room: "day",
+        order: 3,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Day/all14.png",
+        room: "day",
+        order: 4,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Day/all24.jpg",
+        room: "day",
+        order: 5,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Day/all25.jpg",
+        room: "day",
+        order: 6,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Day/all28.png",
+        room: "day",
+        order: 7,
+        title: "",
+        caption: "",
+        date: ""
+    },
+
+    // Night Room
+    {
+        image: "Night/all11.png",
+        room: "night",
+        order: 1,
+        title: "",
+        caption: "",
+        date: ""
+    }
+];
