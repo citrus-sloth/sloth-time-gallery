791bd35e68916421f6fcadf0f3f3afb7a5b824da
citrus-sloth <citrus-sloth@users.noreply.github.com>
2026-07-27 17:28:27 +0900

    Update about section and add new artworks

--- gallery-data.js
+++ gallery-data.js
@@ -4,9 +4,25 @@
 const galleryData = [
     // Morning Room
     {
-        image: "Morning/all36.png",
+        image: "Morning/all40.png",
         room: "morning",
         order: 1,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Morning/all39.png",
+        room: "morning",
+        order: 2,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Morning/all36.png",
+        room: "morning",
+        order: 3,
         title: "",
         caption: "",
         date: ""
@@ -14,7 +30,7 @@ const galleryData = [
     {
         image: "Morning/all35.png",
         room: "morning",
-        order: 2,
+        order: 4,
         title: "",
         caption: "",
         date: ""
@@ -22,7 +38,7 @@ const galleryData = [
     {
         image: "Morning/all34.png",
         room: "morning",
-        order: 3,
+        order: 5,
         title: "",
         caption: "",
         date: ""
@@ -30,7 +46,7 @@ const galleryData = [
     {
         image: "Morning/all09.png",
         room: "morning",
-        order: 4,
+        order: 6,
         title: "",
         caption: "",
         date: ""
@@ -38,7 +54,7 @@ const galleryData = [
     {
         image: "Morning/all10.png",
         room: "morning",
-        order: 5,
+        order: 7,
         title: "",
         caption: "",
         date: ""
@@ -46,7 +62,7 @@ const galleryData = [
     {
         image: "Morning/all12.png",
         room: "morning",
-        order: 6,
+        order: 8,
         title: "",
         caption: "",
         date: ""
@@ -54,7 +70,7 @@ const galleryData = [
     {
         image: "Morning/all16.jpg",
         room: "morning",
-        order: 7,
+        order: 9,
         title: "",
         caption: "",
         date: ""
@@ -62,7 +78,7 @@ const galleryData = [
     {
         image: "Morning/all17.jpg",
         room: "morning",
-        order: 8,
+        order: 10,
         title: "",
         caption: "",
         date: ""
@@ -70,7 +86,7 @@ const galleryData = [
     {
         image: "Morning/all18.jpg",
         room: "morning",
-        order: 9,
+        order: 11,
         title: "",
         caption: "",
         date: ""
@@ -78,7 +94,7 @@ const galleryData = [
     {
         image: "Morning/all19.jpg",
         room: "morning",
-        order: 10,
+        order: 12,
         title: "",
         caption: "",
         date: ""
@@ -86,7 +102,7 @@ const galleryData = [
     {
         image: "Morning/all20.jpg",
         room: "morning",
-        order: 11,
+        order: 13,
         title: "",
         caption: "",
         date: ""
@@ -94,7 +110,7 @@ const galleryData = [
     {
         image: "Morning/all21.jpg",
         room: "morning",
-        order: 12,
+        order: 14,
         title: "",
         caption: "",
         date: ""
@@ -102,7 +118,7 @@ const galleryData = [
     {
         image: "Morning/all22.jpg",
         room: "morning",
-        order: 13,
+        order: 15,
         title: "",
         caption: "",
         date: ""
@@ -110,7 +126,7 @@ const galleryData = [
     {
         image: "Morning/all23.jpg",
         room: "morning",
-        order: 14,
+        order: 16,
         title: "",
         caption: "",
         date: ""
@@ -118,7 +134,7 @@ const galleryData = [
     {
         image: "Morning/all26.png",
         room: "morning",
-        order: 15,
+        order: 17,
         title: "",
         caption: "",
         date: ""
@@ -126,7 +142,7 @@ const galleryData = [
     {
         image: "Morning/all27.png",
         room: "morning",
-        order: 16,
+        order: 18,
         title: "",
         caption: "",
         date: ""
@@ -134,7 +150,7 @@ const galleryData = [
     {
         image: "Morning/all29.png",
         room: "morning",
-        order: 17,
+        order: 19,
         title: "",
         caption: "",
         date: ""
@@ -142,7 +158,7 @@ const galleryData = [
     {
         image: "Morning/all30.png",
         room: "morning",
-        order: 18,
+        order: 20,
         title: "",
         caption: "",
         date: ""
@@ -150,7 +166,15 @@ const galleryData = [
     {
         image: "Morning/all31.png",
         room: "morning",
-        order: 19,
+        order: 21,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Morning/all32.png",
+        room: "morning",
+        order: 22,
         title: "",
         caption: "",
         date: ""
@@ -158,7 +182,7 @@ const galleryData = [
 
     // Day Room
     {
-        image: "Day/all37.png",
+        image: "Day/all38.png",
         room: "day",
         order: 1,
         title: "",
@@ -166,7 +190,7 @@ const galleryData = [
         date: ""
     },
     {
-        image: "Day/all01.jpeg",
+        image: "Day/all37.png",
         room: "day",
         order: 2,
         title: "",
@@ -174,7 +198,7 @@ const galleryData = [
         date: ""
     },
     {
-        image: "Day/all15.png",
+        image: "Day/all01.jpeg",
         room: "day",
         order: 3,
         title: "",
@@ -182,7 +206,7 @@ const galleryData = [
         date: ""
     },
     {
-        image: "Day/all13.png",
+        image: "Day/all15.png",
         room: "day",
         order: 4,
         title: "",
@@ -190,7 +214,7 @@ const galleryData = [
         date: ""
     },
     {
-        image: "Day/all14.png",
+        image: "Day/all13.png",
         room: "day",
         order: 5,
         title: "",
@@ -198,7 +222,7 @@ const galleryData = [
         date: ""
     },
     {
-        image: "Day/all24.jpg",
+        image: "Day/all14.png",
         room: "day",
         order: 6,
         title: "",
@@ -206,7 +230,7 @@ const galleryData = [
         date: ""
     },
     {
-        image: "Day/all25.jpg",
+        image: "Day/all24.jpg",
         room: "day",
         order: 7,
         title: "",
@@ -214,9 +238,17 @@ const galleryData = [
         date: ""
     },
     {
-        image: "Day/all28.png",
+        image: "Day/all25.jpg",
         room: "day",
         order: 8,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Day/all28.png",
+        room: "day",
+        order: 9,
         title: "",
         caption: "",
         date: ""
@@ -224,9 +256,17 @@ const galleryData = [
 
     // Night Room
     {
-        image: "Night/all11.png",
+        image: "Night/all41.png",
         room: "night",
         order: 1,
+        title: "",
+        caption: "",
+        date: ""
+    },
+    {
+        image: "Night/all11.png",
+        room: "night",
+        order: 2,
         title: "",
         caption: "",
         date: ""
